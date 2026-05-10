f4b7c9524a748302716d0e17782230a39ebbe048
MillieBea <milena.wzietek@gmail.com>
2026-05-10 20:04:02 +0200

    Further adapted some CSS styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function displayWord(response) {
   console.log("word generated");
 
+  const newWordElement = document.querySelector("#word");
+
   const answer = response.data.answer;
   const parts = answer.split(/Definition:|Example:/);
   const wordAndPos = parts[0].match(/Word: (.+) \((.+)\)/);
@@ -8,9 +10,13 @@ function displayWord(response) {
   const example = parts[2].trim();
 
   const word = wordAndPos[1].trim(); // The invented word
-  const partOfSpeech = wordAndPos[2].trim(); // The part of speech
+  const partOfSpeech = wordAndPos[2].trim();
+
+  // The part of speech
 
   console.log(`Word: ${word}, Part of Speech: ${partOfSpeech}`);
+
+  newWordElement.classList.remove("ink-bleed");
 
   const typewriterWord = new Typewriter("#word", {
     autoStart: true,
@@ -54,6 +60,22 @@ function generateWordAndDefinitionAndExample(event) {
     "You are an Oxford English Dictionary creative lexicographer for the modern soul. Generate a new word, specify it's part of speech, it's definition and example to use in sentence. Make sure to follow the user instructions when generating the new word";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
+  let resultContainer = document.querySelector("#result-container");
+
+  let newWordElement = document.querySelector("#word");
+  let definitionElement = document.querySelector("#definition");
+  let exampleElement = document.querySelector("#example");
+  resultContainer.classList.remove("hidden");
+
+  newWordElement.innerHTML = `
+  <span class="loading-message">
+    🔎 Consulting the etymology archives about: ${instructionsInput.value}...
+  </span>
+`;
+  newWordElement.classList.add("ink-bleed");
+  definitionElement.innerHTML = "";
+  exampleElement.innerHTML = "";
+
   console.log("generating word");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
@@ -62,3 +84,8 @@ function generateWordAndDefinitionAndExample(event) {
 
 let wordFormElement = document.querySelector("#word-generator-form");
 wordFormElement.addEventListener("submit", generateWordAndDefinitionAndExample);
+
+window.addEventListener("DOMContentLoaded", () => {
+  const video = document.querySelector("#background-video");
+  video.playbackRate = 1.5;
+});
